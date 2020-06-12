7dcc9f6f1cabb928e97cfbf413a967bdf1c77890
Steven Waldron <stevenwaldron@Stevens-MBP.home>
2020-06-11 22:51:01 -0400

    deployment MVP1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express');
 
 const App = require('./server');
 
-const PORT = 3000;
+const PORT = process.env.PORT ||  3000;
 
 App.listen(PORT, ()=> {
     console.log(`listening on http://localhost:${PORT}`)
